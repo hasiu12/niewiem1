d40d6521bc5c58d0f354baa846a3516bb477cd80
hasiu12 <131602193+hasiu12@users.noreply.github.com>
2023-04-23 18:45:14 +0200

    ?

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let currentQuestion = 0;
 let userAnswer;
 let quizData;
 let answerChecked = false;
+let correctAnswers = 0;
+let wrongAnswers = 0;
 
 async function fetchData() {
     const response = await fetch('quiz_data.json');
@@ -61,32 +63,33 @@ document.getElementById('submit').addEventListener('click', () => {
 });
 
 function checkAnswer() {
-    const resultElement = document.getElementById('result');
     const correctAnswer = quizData[currentQuestion].correctAnswer;
 
     if (userAnswer === correctAnswer) {
-        resultElement.textContent = 'Poprawna odpowiedź!';
+        correctAnswers++;
     } else {
-        resultElement.textContent = `Zła odpowiedź! Poprawna odpowiedź to: ${quizData[currentQuestion].answers[correctAnswer]}`;
+        wrongAnswers++;
     }
 
-    setTimeout(() => {
-        if (currentQuestion < quizData.length) {
-            displayQuestion();
-            resetAnswer();
+    if (currentQuestion < quizData.length - 1) {
+        currentQuestion++;
+        displayQuestion();
+        resetAnswer();
+        answerChecked = false;
+    } else {
+        const resultElement = document.getElementById('result');
+        resultElement.textContent = `Quiz zakończony! Poprawne odpowiedzi: ${correctAnswers}, błędne odpowiedzi: ${wrongAnswers}.`;
+
+        setTimeout(() => {
+            document.getElementById('quizContent').style.display = 'none';
+            document.getElementById('startQuiz').style.display = 'block';
+            currentQuestion = 0;
+            correctAnswers = 0;
+            wrongAnswers = 0;
             resultElement.textContent = '';
             answerChecked = false;
-        } else {
-            resultElement.textContent += ' To już koniec quizu!';
-            setTimeout(() => {
-                document.getElementById('quizContent').style.display = 'none';
-                document.getElementById('startQuiz').style.display = 'block';
-                currentQuestion = 0;
-                resultElement.textContent = '';
-                answerChecked = false;
-            }, 3000);
-        }
-    }, 3000);
+        }, 3000);
+    }
 }
 
 
